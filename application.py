52b058997ed0be279fc7c6e9d979b16c41b7f9a8
FreddieCOLAB <Freddie@colabsoftware.com>
2018-05-07 20:51:45 -0230

    Initializing boilerplate for chat app.

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# The main route, which will direct users to go to the chatroom.
+@app.route("/")
+def index():
+    names = ["Tyler", "Fred", "Brian"]
+    return render_template("index.html", names=names)
+
+
+# The chatroom, where users will send messages to each other.
+@app.route("/chat_room")
+def chat_room():
+    return render_template("chat_room.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
